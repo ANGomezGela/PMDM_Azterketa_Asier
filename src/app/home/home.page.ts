cfb38a8c40eec81f3636d32e882fb9bf58bd2ea7
Asier N <agomeza23@fpsanturtzilh.eus>
2025-02-13 12:57:15 +0100

    Ultima version

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CuestionarioService } from './../servicios/cuestionario.service';
 import { IPregunta } from './../interfaces/interfaces';
+import { ModuloModule } from '../Komponenteak/modulo.module';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,42 @@ import { IPregunta } from './../interfaces/interfaces';
 })
 export class HomePage {
 
+  Berriak: IPregunta[] = [];
+
   //Zerbitzua inportatu
-  constructor() {}
+  constructor(private service: CuestionarioService, private alertController: AlertController) {}
+
+  ngOnInit(): void {  
+    this.getBerriak();
+    console.log(this.Berriak);
+  }
+
+  getBerriak(){
+    this.Berriak = this.service.getDatuak();
+  }
+
+  async presentAlert(i: number){
+    
+    const alert = await this.alertController.create({
+      header: "¿De qué marca es este logotipo?",
+      inputs: [{
+        id: 'respuesta',
+        name: 'respuesta',
+        placeholder: 'Nombre de logo',
+      },],
+      buttons:[{
+        id: 'enviar',
+        text: 'enviar'
+      }]
+    })
+    await alert.present();
+  }
+  
 
   //Metodo bat sortu "Erantzun" onclick egiteko
+  OnClick(){
+    
+  }
   //IGaldera bat jasoko du eta zerbitzua deituko du beharrezkoak diren eragiketak egiteko
 
   //Sortu metodo bat "Gorde"ren onclick-a kudeatzeko
--- src/app/servicios/cuestionario.service.ts
+++ src/app/servicios/cuestionario.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 import { IPregunta } from './../interfaces/interfaces';
 import { Observable } from 'rxjs';
@@ -10,11 +10,29 @@ import { Injectable } from '@angular/core';
 export class CuestionarioService {
   // Array bat gordetzeko json-ean dauden galdera guztiak. Gogoratu array-a abiarazten arazoak ekiditzeko
   // Gehitu beharrezkoak diren konponenteak eta zerbitzuak
-  constructor() {
+
+  datuak: IPregunta[] = [];
+
+  constructor(private http: HttpClient, private alertController: AlertController) {
     //Datuak kargatu
+    let datuakFitx:Observable<IPregunta[]>;
+    datuakFitx = this.http.get<IPregunta[]>("/assets/datos/datos.json");
+    datuakFitx.subscribe(IPregunta => {
+      console.log(IPregunta);
+      this.datuak.push(...IPregunta); //sartzeko array bat beste array batean
+    });
+    
   }
 
+
+  
+  
   // IPregunta array-a bueltatuko duen metodoa, hau da, galdetegiko galdera guztiak array batean
+  getDatuak(){
+    console.log(this.datuak);
+    return this.datuak;
+  }
+    
 
 
   // Fitxategia irakurtzeko metodoa
